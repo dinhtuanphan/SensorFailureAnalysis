983daefcd2083fb94aabe59c836ebd0fd891368a
dinhtuanphan <phandinhtuans@gmail.com>
2021-05-25 11:39:41 -0400

    Add files

--- sensor.R
+++ sensor.R
@@ -12,3 +12,34 @@ str(sensor)
 typeof(sensor)
 summary(sensor)
 head(sensor)
+
+# create an empty vector to store data
+
+sumData <- numeric()
+sumPass <- numeric()
+
+# Run basic exploratory data analysis
+newType <- sensor[sensor$`Adhesive Type` == "Low Viscosity" &
+                 sensor$`Sensor Surface` == "Trench" &
+                 sensor$`Filling Method` == "Pin" &
+                sensor$`Working Resistance` == "< 2",]
+
+flatPass <- subset(flat, flat$`Calibration Day 0` == "Pass")
+
+sumData[1] <- nrow(flat)
+sumPass[1] <- nrow(flatPass)
+
+# flatSummary <- table(flat$`Calibration Day 0`)
+
+trench <- sensor[sensor$`Adhesive Type` == "Low Viscosity" &
+                   sensor$`Sensor Surface` == "Trench",]
+trenchPass <- subset(trench, trench$`Calibration Day 0` == "Pass")
+sumData[2] <- nrow(trench)
+sumPass[2] <- nrow(trenchPass)
+
+# trenchSummary <- table(trench$`Calibration Day 0`)
+
+plot(sumPass)
+
+
+
